b1577147e1f6cdb3c3dae464cfd2492c146212e0
Kalhara Ediriweera <Kavindukalharaofficial@gmail.com>
2026-07-01 01:06:26 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://portfolio-backend-rho-indol.vercel.app';
 
 /**
  * Helper to fetch headers with authentication tokens automatically.
